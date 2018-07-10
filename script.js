c3f92896d198211728072b87536d5fcae3ff7ae9
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-07-10 12:09:45 +0200

    changing colors working

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("elo");
+let container = document.getElementById("container");
+let squaresAmount = 16 * 16;
+
+let createSquare = () => {
+  let square = document.createElement("div");
+  square.classList.add("new_div");
+  container.appendChild(square);
+  return square;
+};
+
+console.log(createSquare());
+
+createSquares = () => {
+  for (i = 0; i < squaresAmount - 1; i++) {
+    container.appendChild(createSquare());
+  }
+};
+
+createSquares();
+
+let squaresArray = document.querySelectorAll(".new_div");
+
+squaresArray.forEach(square => {
+  square.addEventListener("mouseover", function(square) {
+    this.classList.add("changed_color");
+  });
+});
