f73e48a02c04b600671dc666f3008b1580af71a9
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-07-11 09:30:16 +0200

    colors toggling

--- script.js
+++ script.js
@@ -13,10 +13,84 @@ let createGrid = sideSize => {
     container.appendChild(square);
   }
 };
-
-console.log(document.styleSheets[0].cssRules[1]);
+//load starting grid
 
 createGrid(16);
+
+//add hover
+
+onHover = () => {
+  let squaresArray = Array.from(document.querySelectorAll(".new_div"));
+  for (i = 0; i < squaresArray.length; i++) {
+    squaresArray[i].addEventListener("mouseover", function(e) {
+      //change css value based on current value
+      this.style.opacity
+        ? (this.style.opacity = parseFloat(this.style.opacity) + 0.1)
+        : (this.style.opacity = 0.1);
+    });
+  }
+};
+
+//apply listeners to initial grid
+onHover();
+
+//clear board on click
+let buttonClear = document.getElementById("reset");
+buttonClear.addEventListener("click", e => {
+  let newSize = Number(window.prompt("How many squares per side?", 16));
+  //only if new size is provided remove old grid
+  if (newSize) {
+    while (container.hasChildNodes()) {
+      container.removeChild(container.lastChild);
+    }
+    //create new grid from size
+    createGrid(newSize);
+    onHover();
+    color ? colorful() : "";
+  }
+});
+
+// toggle B/W and color pixels
+document.querySelector("#color_change").addEventListener("click", e => {
+  if (color) {
+    color = 0;
+    bw();
+  } else {
+    color = 1;
+    colourful();
+  }
+});
+
+//add color to pixels
+function colourful() {
+  let squares = Array.from(document.querySelectorAll(".new_div"));
+  const hexSymbol = "0123456789abcdef";
+
+  // loop through each pixel
+  for (i = 0; i < squares.length; i++) {
+    let color = "#";
+    squares[i].style.opacity = 0;
+    //create a random hexcode for each pixel
+    for (var j = 0; j < 6; j++) {
+      color += hexSymbol.charAt(Math.floor(Math.random() * hexSymbol.length));
+    }
+    //set pixel to new color
+    console.log(Math.floor(Math.random() * hexSymbol.length));
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+// remove color from pixels
+function bw() {
+  let squares = Array.from(document.querySelectorAll(".new_div"));
+  for (i = 0; i < squares.length; i++) {
+    squares[i].style.opacity = 0;
+    squares[i].style.backgroundColor = "black";
+  }
+}
+
+//before refactoring
+
 /*
 //create one square
 let createSquare = () => {
@@ -33,9 +107,8 @@ createSquares = squaresPerSide => {
   }
 };
 
-createSquares(squaresPerSide); */
+createSquares(squaresPerSide); 
 
-//add hover
 let squaresArray = document.querySelectorAll(".new_div");
 
 //first layout
@@ -43,7 +116,7 @@ let squaresArray = document.querySelectorAll(".new_div");
   square.addEventListener("mouseover", function(square) {
     this.classList.add("changed_color");
   });
-});*/
+});
 
 //second layout
 squaresArray.forEach(square => {
@@ -72,5 +145,5 @@ buttonClear.addEventListener("click", function() {
     square.addEventListener("mouseover", function(square) {
       this.classList.add("changed_color");
     });
-  });*/
-});
+  });
+}); */
